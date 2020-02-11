737a2dd1256a46068780df81975d718317367c76
Steven G <steventheworker@gmail.com>
2020-02-11 03:26:39 -0800

    add frankenquery, make mobile friendlier?

--- server/client/app.js
+++ server/client/app.js
@@ -1,16 +1,32 @@
-var app = {};
-app.init = function() {
-    app.initializeDom();
-};
+const app = {};
 app.send = function(data) {
     if (!app.socket) return;
     app.socket.send(data);
     console.log("=>\""+data+'"');
 };
-app.initializeDom = function() {
-    document.body.onkeydown = function(e) {
-        if (e.code === "WakeUp") return;
-        app.send('k|' + e.code);
-    };
+app.listenKeys = function(e) {
+    console.log(e.type)
+    const char = e.data;
+    let key = e.code || (char && char.charCodeAt(0));
+    if (e.type === "keydown") $('#is_mobile').attr('disabled', true);
+    /*
+    alert(e.key + '::' + e.which)
+    const key = e.code || e.keyCode || e.charCode;
+    alert(key)
+    */
+    if (!key || key === "WakeUp") return; //python library has no fn (aka WakeUp)
+    app.send('k|' + key);
 };
-window.onload = app.init;
+
+app.initializeDom = function() {
+    $('body')
+    .keydown(app.listenKeys)
+    .on("textInput", "#is_mobile", app.listenKeys)
+    .on('click touchend', function() {
+        $('#is_mobile')[0].focus();
+    });
+};
+
+$(function() {
+    app.initializeDom();
+});
--- server/events.js
+++ server/events.js
@@ -2,6 +2,7 @@ let {PythonShell} = require('python-shell')
 
 module.exports = {
     'k': function(socket, data) {
+        if (!data) return;
         PythonShell.run('./controls/press.py',
         {
             args: [data]
